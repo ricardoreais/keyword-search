c1062ad60098ae05ebf84275c078e42c1dbcab30
Ricardo Reais <ricardoreais@users.noreply.github.com>
2026-07-05 13:11:23 +0100

    fix: update title, favicon, route, and search input behavior

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { IEntry } from '../common/models/IEntry';
 import { EntryService } from './services/entry.service';
@@ -11,7 +10,7 @@ import { HighlightSearchPipe } from '../common/pipes/highlight-search-pipe';
   standalone: true,
   selector: 'app-test1',
   templateUrl: './test1.component.html',
-  imports: [AsyncPipe, FormsModule, NgxPaginationModule, HighlightSearchPipe]
+  imports: [AsyncPipe, NgxPaginationModule, HighlightSearchPipe]
 })
 export class Test1Component implements OnInit {
   private entryService = inject(EntryService);
@@ -21,19 +20,14 @@ export class Test1Component implements OnInit {
   entriesCount = 50000;
   page = 1;
   pageSize = 20;
-  timeoutHandle!: number;
 
   ngOnInit() {
     this.entryService.getEntries(this.entriesCount);
     this.filteredData$ = this.entryService.getFilteredEntries('');
   }
 
-  updateFilter() {
-    if (!this.filter || this.filter.length > 0) {
-      window.clearTimeout(this.timeoutHandle);
-    }
-    this.timeoutHandle = window.setTimeout(() => {
-      this.filteredData$ = this.entryService.getFilteredEntries(this.filter);
-    }, 300);
+  onInput(event: Event) {
+    this.filter = (event.target as HTMLInputElement).value;
+    this.filteredData$ = this.entryService.getFilteredEntries(this.filter);
   }
 }
